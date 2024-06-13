1d4ef1c33d4dd5fc3aa9be6c02b8f981291513ab
tomato57 <kishore5790@gmail.com>
2024-06-13 23:53:12 +0530

    Update viz_downloader.js

--- viz_downloader.js
+++ viz_downloader.js
@@ -1,15 +1,15 @@
 /*
 PREREQUISITES
 1. open chrome://settings/downloads
-   - set download location
+   - change the download location
    - disable "ask where to save each file before downloading"
-2. add extn to overwrite preexisting file instead of adding unwanted suffix to filename
+2. add below extn to overwrite preexisting file instead of adding unwanted suffix to filename
    - https://chromewebstore.google.com/detail/downloads-overwrite-alrea/lddjgfpjnifpeondafidennlcfagekbp
 
 USAGE
 1. load the viz chapter and open developer console
 2. Run the following
-   import("https://cdn.jsdelivr.net/gh/tomato57/viz-downloader@v1.5.0/viz_downloader.js").then(function(module) {
+   import("https://cdn.jsdelivr.net/gh/tomato57/viz-downloader@v1.6.0/viz_downloader.js").then(function(module) {
        module.downloadChapter()()
    })
 */
@@ -50,11 +50,15 @@ export const buildProcessChain = (processList) => {
     }
     return processChain
 }
+export const getChapterNum = () => {
+    return document.title.match(/Chapter\s(\d+)\sManga/)[1]
+}
 export const getCurrentPage = () => {
     return parseInt(document.getElementsByClassName("page_slider_label center")[0].textContent.match(/Pages?:\s(\d+)/)[1])
 }
 export const getMaxPage = () => {
-    return parseInt(document.getElementsByClassName("page_slider_label left")[0].textContent.match(/Page\s(\d+)/)[1])
+    // skip ad pages
+    return parseInt(document.getElementsByClassName("page_slider_label left")[0].textContent.match(/Page\s(\d+)/)[1]) - 4
 }
 export const goLeft = () => {
     document.dispatchEvent(
@@ -75,22 +79,31 @@ export const goRight = () => {
 export const downloadCurrentPage = () => {
     let leftCanvas = document.getElementById("canvas_left_current")
     let rightCanvas = document.getElementById("canvas_right_current")
-    // skip last pages with ads
-    if (window.getComputedStyle(leftCanvas).getPropertyValue("left") == "0px") {
-        let combined = document.createElement("canvas")
-        combined.width = leftCanvas.width + rightCanvas.width
-        combined.height = leftCanvas.height
-        let context = combined.getContext("2d")
-        context.drawImage(leftCanvas, 0, 0)
-        context.drawImage(rightCanvas, leftCanvas.width, 0)
-        let image = combined.toDataURL("image/png").replace("image/png", "image/octet-stream")
-        let chapter = document.title.match(/Chapter\s(\d+)\sManga/)[1]
-        let page = getCurrentPage()
-        let link = document.createElement("a")
-        link.setAttribute("download", `${chapter}_${page}.png`)
-        link.setAttribute("href", image)
-        link.click()
+    let combined = document.createElement("canvas")
+    combined.width = leftCanvas.width + rightCanvas.width
+    combined.height = leftCanvas.height
+    let context = combined.getContext("2d")
+    context.drawImage(leftCanvas, 0, 0)
+    context.drawImage(rightCanvas, leftCanvas.width, 0)
+    let image = combined.toDataURL("image/png").replace("image/png", "image/octet-stream")
+    const chapterNum = getChapterNum()
+    const currentPage = getCurrentPage()
+    let link = document.createElement("a")
+    link.setAttribute("download", `${chapterNum}_${currentPage}.png`)
+    link.setAttribute("href", image)
+    link.click()
+}
+export const downloadInfoFile = () => {
+    const chapterNum = getChapterNum()
+    const maxPage = getMaxPage()
+    let info = {
+       "maxPage": maxPage,
     }
+    const infoText = JSON.stringify(info)
+    let link = document.createElement("a")
+    link.setAttribute("download", `${chapterNum}_info.json`)
+    link.setAttribute("href", "data:text/plain;charset=utf-8," + encodeURIComponent(infoText))
+    link.click()
 }
 export const downloadChapter = ({
     goLeftTimeout = 3000,
@@ -115,6 +128,7 @@ export const downloadChapter = ({
         processList[index++] = (processChain) => addSleepToProcessChain(processChain, goLeftTimeout)
         processList[index++] = (processChain) => addFuncToProcessChain(processChain, downloadCurrentPage)
     }
+    processList[index++] = (processChain) => addFuncToProcessChain(processChain, downloadInfoFile)
     return buildProcessChain(processList)
 }
 // downloadChapter()()
